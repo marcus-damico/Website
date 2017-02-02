6168b23550318e46d887c5e88c76dcbd8b3e363d
Marcus <mdmdamico@gmail.com>
2017-02-01 21:06:50 -0500

    Nada

--- phase10/phase10.js
+++ phase10/phase10.js
@@ -1,17 +1,3 @@
-var numBox1=document.getElementById("numBox1");
-var numBox2=document.getElementById("numBox2");
-var numBox3=document.getElementById("numBox3");
-var numBox4=document.getElementById("numBox4");
-var numBox5=document.getElementById("numBox5");
-var numBox6=document.getElementById("numBox6");
-var ttlBox1=document.getElementById("ttlBox1");
-var ttlBox2=document.getElementById("ttlBox2");
-var ttlBox3=document.getElementById("ttlBox3");
-var ttlBox4=document.getElementById("ttlBox4");
-var ttlBox5=document.getElementById("ttlBox5");
-var ttlBox6=document.getElementById("ttlBox6");
-var clrBttn=document.getElementById("clrBttn");
-var enterButton=document.getElementById("ntrBttn");
 var c=document.getElementById("p10Canvas");
 var ctx=c.getContext("2d");
 
@@ -19,11 +5,28 @@ ctx.font="30px serif";
 ctx.fillText("Phase10 Application",250,50);
 
 function addFunction(n1, n2, n3, n4, n5, n6, t1, t2, t3, t4, t5, t6){
-	t1 = n1 + t1;
-	t2 = n2 + t2;
-	t3 = n3 + t3;
-	t4 = n4 + t4;
-	t5 = n5 + t5;
-	t6 = n6 + t6;
-	return t1, t2, t3, t4, t5, t6;
+	var temp;
+	temp = parseInt(t1.value || 0);
+    temp += parseInt(n1);
+    t1.value = temp;
+	
+	temp = parseInt(t2.value || 0);
+	temp += parseInt(n2);
+	t2.value = temp;
+
+	temp = parseInt(t3.value || 0);
+	temp += parseInt(n3);
+	t3.value = temp;
+
+	temp = parseInt(t4.value || 0);
+	temp += parseInt(n4);
+	t4.value = temp;
+
+	temp = parseInt(t5.value || 0);
+	temp += parseInt(n5);
+	t5.value = temp;
+
+	temp = parseInt(t6.value || 0);
+	temp += parseInt(n6);
+	t6.value = temp;
 }
