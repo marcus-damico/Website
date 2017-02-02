16d3b8e490bd2626ac6ebba90cd5a25206ce29f9
Marcus <mdmdamico@gmail.com>
2017-02-01 21:17:02 -0500

    Like This?

--- phase10/phase10.js
+++ phase10/phase10.js
@@ -7,27 +7,27 @@ ctx.fillText("Phase10 Application",250,50);
 function addFunction(n1, n2, n3, n4, n5, n6, t1, t2, t3, t4, t5, t6){
 	var temp;
 	temp = parseInt(t1.value || 0);
-    temp += parseInt(n1);
+    temp += parseInt(n1.value);
     t1.value = temp;
 	
 	temp = parseInt(t2.value || 0);
-	temp += parseInt(n2);
+	temp += parseInt(n2.value);
 	t2.value = temp;
 
 	temp = parseInt(t3.value || 0);
-	temp += parseInt(n3);
+	temp += parseInt(n3.value);
 	t3.value = temp;
 
 	temp = parseInt(t4.value || 0);
-	temp += parseInt(n4);
+	temp += parseInt(n4.value);
 	t4.value = temp;
 
 	temp = parseInt(t5.value || 0);
-	temp += parseInt(n5);
+	temp += parseInt(n5.value);
 	t5.value = temp;
 
 	temp = parseInt(t6.value || 0);
-	temp += parseInt(n6);
+	temp += parseInt(n6.value);
 	t6.value = temp;
 
 	n1.value = 0;
