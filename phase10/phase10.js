30c6b713f0d6b7bb141e684b110c57b924d2afd6
Marcus <mdmdamico@gmail.com>
2017-02-07 17:43:07 -0500

    Enter to add up score
    You can hit enter to add up the scores.
    I definitely didn’t do it the most efficient way but oh well.

--- phase10/phase10.js
+++ phase10/phase10.js
@@ -4,8 +4,16 @@ var ctx=c.getContext("2d");
 ctx.font="30px serif";
 ctx.fillText("Phase10 Application",250,50);
 
-function addFunction(n1, n2, n3, n4, n5, n6, t1, t2, t3, t4, t5, t6){
+//I want this to allow the enter button to trigger the addFunction
+
+var ntr = document.getElementById("ntrButton");
+
+function handle(e, n1, n2, n3, n4, n5, n6, t1, t2, t3, t4, t5, t6){
 	var temp;
+
+	if (e.keyCode == 13) {
+		e.preventDefault();
+
 	temp = parseInt(t1.value || 0);
     temp += parseInt(n1.value);
     t1.value = temp;
@@ -36,5 +44,42 @@ function addFunction(n1, n2, n3, n4, n5, n6, t1, t2, t3, t4, t5, t6){
 	n4.value = 0;
 	n5.value = 0;
 	n6.value = 0;
+	}
+}
+
+//Calculates the numbers into a textbox and resets the input values
+function addFunction(n11, n21, n31, n41, n51, n61, t11, t21, t31, t41, t51, t61){
+
+	var temp;
+	temp = parseInt(t11.value || 0);
+    temp += parseInt(n11.value);
+    t11.value = temp;
+	
+	temp = parseInt(t21.value || 0);
+	temp += parseInt(n21.value);
+	t21.value = temp;
+
+	temp = parseInt(t31.value || 0);
+	temp += parseInt(n31.value);
+	t31.value = temp;
+
+	temp = parseInt(t41.value || 0);
+	temp += parseInt(n41.value);
+	t41.value = temp;
+
+	temp = parseInt(t51.value || 0);
+	temp += parseInt(n51.value);
+	t51.value = temp;
+
+	temp = parseInt(t61.value || 0);
+	temp += parseInt(n61.value);
+	t61.value = temp;
+
+	n11.value = 0;
+	n21.value = 0;
+	n31.value = 0;
+	n41.value = 0;
+	n51.value = 0;
+	n61.value = 0;
 
 }
